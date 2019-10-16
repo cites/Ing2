6a3a8e8ab6919cbc6bab65e1d99f107f7621c3ad
Oscar <pintososcar@gmail.com>
2019-10-16 16:46:03 -0300

    Estructura

---
+++ sysReclamo/src/sysReclamo/Perfil.java
@@ -0,0 +1,5 @@
+package sysReclamo;
+
+public enum Perfil {
+
+}
---
+++ sysReclamo/src/sysReclamo/Testciudadano.java
@@ -0,0 +1,14 @@
+package sysReclamo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Testciudadano {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
